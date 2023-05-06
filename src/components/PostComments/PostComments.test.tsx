5093087e855315e89a15b79b84604467d3f5f4f1
Rafael <rafael_guilherme@hotmai.com>
2023-05-06 12:14:37 -0300

    teste add 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+//import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +7,26 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar dois comentários novos', () => {
+        const { debug } = render(<PostComment/>)
+        const comentarioNovo = screen.getByTestId('texto-comentario')
+        const botao = screen.getByTestId('btn-adcionar-comentario')
+        fireEvent.change(comentarioNovo, {
+            target: {
+                value: 'Comentário 1'
+            }
+        })
+        fireEvent.click(botao)
+
+        fireEvent.change(comentarioNovo, {
+            target: {
+                value: 'Comentário 2'
+            }
+        })
+        fireEvent.click(botao)
+        debug()
+
+        expect(screen.getAllByTestId('lista-comentario')).toHaveLength(2)
+    })
 });
